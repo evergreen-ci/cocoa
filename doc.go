6365c468cbc40cee5645ad840f05fc2b1288511d
Kim Tao <kim.tao@mongodb.com>
2021-06-09 10:30:08 -0400

    Update the package doc

--- doc.go
+++ doc.go
@@ -3,12 +3,14 @@ Package cocoa provides interfaces to interact with groups of containers (called
 pods) backed by container orchestration services. Containers are not managed
 individually - they're managed as logical groupings of containers.
 
-The ECSPodManager interface provides an abstraction to interact with pods backed
-by ECS without needing to make direct calls to the API to perform
-frequently-used operations.
+The ECSPodCreator interface provides an abstraction to create pods backed by ECS
+without needing to make direct calls to the API.
+
+The ECSPod is a self-contained unit that allows users to manage their pod
+without having to make direct calls to the API. It is backed by an ECSClient.
 
 The ECSClient interface provides a convenience wrapper around the ECS API. If
-the ECSPodManager does not fulfill your needs, you can make API calls directly
-to ECS instead.
+the ECSPodCreator and ECSPod does not fulfill your needs, you can make API calls
+directly to ECS instead.
 */
 package cocoa
