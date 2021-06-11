f0f7bcab231966b3e97f8e3b8f624c2701c29a38
Kim Tao <kim.tao@mongodb.com>
2021-06-11 11:07:51 -0400

    EVG-14709: implement RegisterTaskDefinition and DeregisterTaskDefinition (#4)

--- ecs_client.go
+++ ecs_client.go
@@ -3,10 +3,17 @@ package cocoa
 import (
 	"context"
 
+	"github.com/mongodb/grip"
+	"github.com/mongodb/grip/message"
 	"github.com/pkg/errors"
 
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/awserr"
+	"github.com/aws/aws-sdk-go/aws/request"
+	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/ecs"
 	"github.com/evergreen-ci/cocoa/awsutil"
+	"github.com/evergreen-ci/utility"
 )
 
 // ECSClient provides a common interface to interact with an ECS client and its
@@ -27,28 +34,116 @@ type ECSClient interface {
 // BasicECSClient provides an ECSClient implementation that wraps the ECS API.
 // It supports retrying requests using exponential backoff and jitter.
 type BasicECSClient struct {
-	opts awsutil.ClientOptions
+	ecs     *ecs.ECS
+	opts    *awsutil.ClientOptions
+	session *session.Session
 }
 
 // NewBasicECSClient creates a new ECS client from the given options.
 func NewBasicECSClient(opts awsutil.ClientOptions) (*BasicECSClient, error) {
-	if err := opts.Validate(); err != nil {
-		return nil, errors.Wrap(err, "invalid options")
+	c := &BasicECSClient{
+		opts: &opts,
+	}
+	if err := c.setup(); err != nil {
+		return nil, errors.Wrap(err, "setting up client")
 	}
 
-	return &BasicECSClient{
-		opts: opts,
-	}, nil
+	return c, nil
+}
+
+func (c *BasicECSClient) setup() error {
+	if err := c.opts.Validate(); err != nil {
+		return errors.Wrap(err, "invalid options")
+	}
+
+	if c.ecs != nil {
+		return nil
+	}
+
+	if err := c.setupSession(); err != nil {
+		return errors.Wrap(err, "setting up session")
+	}
+
+	c.ecs = ecs.New(c.session)
+
+	return nil
+}
+
+func (c *BasicECSClient) setupSession() error {
+	if c.session != nil {
+		return nil
+	}
+
+	creds, err := c.opts.GetCredentials()
+	if err != nil {
+		return errors.Wrap(err, "getting credentials")
+	}
+	sess, err := session.NewSession(&aws.Config{
+		HTTPClient:  c.opts.HTTPClient,
+		Region:      c.opts.Region,
+		Credentials: creds,
+	})
+	if err != nil {
+		return errors.Wrap(err, "creating session")
+	}
+
+	c.session = sess
+
+	return nil
 }
 
 // RegisterTaskDefinition registers a new task definition.
-func (c *BasicECSClient) RegisterTaskDefinition(context.Context, *ecs.RegisterTaskDefinitionInput) (*ecs.RegisterTaskDefinitionOutput, error) {
-	return nil, errors.New("TODO: implement")
+func (c *BasicECSClient) RegisterTaskDefinition(ctx context.Context, in *ecs.RegisterTaskDefinitionInput) (*ecs.RegisterTaskDefinitionOutput, error) {
+	if err := c.setup(); err != nil {
+		return nil, errors.Wrap(err, "setting up client")
+	}
+
+	var out *ecs.RegisterTaskDefinitionOutput
+	var err error
+	msg := awsutil.MakeAPILogMessage("RegisterTaskDefinition", in)
+	if err := utility.Retry(ctx,
+		func() (bool, error) {
+			out, err = c.ecs.RegisterTaskDefinitionWithContext(ctx, in)
+			if awsErr, ok := err.(awserr.Error); ok {
+				grip.Debug(message.WrapError(awsErr, msg))
+				switch awsErr.Code() {
+				case request.InvalidParameterErrCode, request.ParamRequiredErrCode:
+					return false, err
+				}
+			}
+			return true, err
+		}, *c.opts.RetryOpts); err != nil {
+		return nil, err
+	}
+
+	return out, err
 }
 
 // DeregisterTaskDefinition deregisters an existing task definition.
-func (c *BasicECSClient) DeregisterTaskDefinition(context.Context, *ecs.DeregisterTaskDefinitionInput) (*ecs.DeregisterTaskDefinitionOutput, error) {
-	return nil, errors.New("TODO: implement")
+func (c *BasicECSClient) DeregisterTaskDefinition(ctx context.Context, in *ecs.DeregisterTaskDefinitionInput) (*ecs.DeregisterTaskDefinitionOutput, error) {
+	if err := c.setup(); err != nil {
+		return nil, errors.Wrap(err, "setting up client")
+	}
+
+	var out *ecs.DeregisterTaskDefinitionOutput
+	var err error
+	msg := awsutil.MakeAPILogMessage("DeregisterTaskDefinition", in)
+	if err := utility.Retry(ctx,
+		func() (bool, error) {
+			out, err = c.ecs.DeregisterTaskDefinitionWithContext(ctx, in)
+			if awsErr, ok := err.(awserr.Error); ok {
+				grip.Debug(message.WrapError(awsErr, msg))
+				switch awsErr.Code() {
+				case request.InvalidParameterErrCode, request.ParamRequiredErrCode:
+					return false, err
+				}
+			}
+			return true, err
+		}, *c.opts.RetryOpts); err != nil {
+		return nil, err
+	}
+
+	return out, err
 }
 
 // RunTask runs a new task.
