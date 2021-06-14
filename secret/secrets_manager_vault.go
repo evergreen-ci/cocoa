6726d36210ac21ed511258a79b0b76648794d3be
Kim Tao <kim.tao@mongodb.com>
2021-06-14 17:17:30 -0400

    EVG-14809: make Secrets Manager vault constructor (#10)

--- secret/secrets_manager_vault.go
+++ secret/secrets_manager_vault.go
@@ -7,7 +7,16 @@ import (
 
 // BasicSecretsManager provides a Vault implementation backed by Amazon Secrets
 // Manager.
-type BasicSecretsManager struct{}
+type BasicSecretsManager struct {
+	client SecretsManagerClient
+}
+
+// NewBasicSecretsManager creates a Vault backed by Secrets Manager.
+func NewBasicSecretsManager(c SecretsManagerClient) *BasicSecretsManager {
+	return &BasicSecretsManager{
+		client: c,
+	}
+}
 
 // CreateSecret creates a new secret.
 func (m *BasicSecretsManager) CreateSecret(ctx context.Context, s NamedSecret) (id string, err error) {
