7f700ba5fdba136e9122511a311a6665e60fe526
Jessica <jessfeng@users.noreply.github.com>
2021-06-23 13:28:21 -0400

    EVG-14789: Implement StopTask and test ECS tasks (#17)

--- ecs_client.go
+++ ecs_client.go
@@ -205,7 +205,28 @@ func (c *BasicECSClient) DescribeTasks(ctx context.Context, in *ecs.DescribeTask
 
 // StopTask stops a running task.
 func (c *BasicECSClient) StopTask(ctx context.Context, in *ecs.StopTaskInput) (*ecs.StopTaskOutput, error) {
-	return nil, errors.New("TODO: implement")
+	if err := c.setup(); err != nil {
+		return nil, errors.Wrap(err, "setting up client")
+	}
+
+	var out *ecs.StopTaskOutput
+	var err error
+	msg := awsutil.MakeAPILogMessage("StopTask", in)
+	if err := utility.Retry(ctx,
+		func() (bool, error) {
+			out, err = c.ecs.StopTaskWithContext(ctx, in)
+			if awsErr, ok := err.(awserr.Error); ok {
+				grip.Debug(message.WrapError(awsErr, msg))
+				switch awsErr.Code() {
+				case request.InvalidParameterErrCode, request.ParamRequiredErrCode:
+					return false, err
+				}
+			}
+			return true, err
+		}, *c.opts.RetryOpts); err != nil {
+		return nil, err
+	}
+	return out, nil
 }
 
 // Close closes the client and cleans up its resources.
--- ecs_client_test.go
+++ ecs_client_test.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"os"
+	"strings"
 	"testing"
 	"time"
 
@@ -16,14 +17,35 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestECSClientInterface(t *testing.T) {
+func TestECSClient(t *testing.T) {
 	assert.Implements(t, (*ECSClient)(nil), &BasicECSClient{})
 }
+func TestECSClientTaskDefinition(t *testing.T) {
 
-func TestECSClientRegisterAndDeregisterTaskDefinition(t *testing.T) {
+	cleanupTaskDefinition := func(ctx context.Context, t *testing.T, c *BasicECSClient, out *ecs.RegisterTaskDefinitionOutput) {
+		if out != nil && out.TaskDefinition != nil && out.TaskDefinition.TaskDefinitionArn != nil {
+			out, err := c.DeregisterTaskDefinition(ctx, &ecs.DeregisterTaskDefinitionInput{
+				TaskDefinition: out.TaskDefinition.TaskDefinitionArn,
+			})
+			require.NoError(t, err)
+			require.NotZero(t, out)
+		}
+	}
+
+	cleanupTask := func(ctx context.Context, t *testing.T, c *BasicECSClient, runOut *ecs.RunTaskOutput) {
+		if runOut != nil && len(runOut.Tasks) > 0 && runOut.Tasks[0].TaskArn != nil {
+			out, err := c.StopTask(ctx, &ecs.StopTaskInput{
+				Cluster: aws.String(os.Getenv("AWS_ECS_CLUSTER")),
+				Task:    aws.String(*runOut.Tasks[0].TaskArn),
+			})
+			require.NoError(t, err)
+			require.NotZero(t, out)
+		}
+	}
+
 	checkAWSEnvVarsForECS(t)
 
-	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
 	hc := utility.GetHTTPClient()
@@ -39,49 +61,188 @@ func TestECSClientRegisterAndDeregisterTaskDefinition(t *testing.T) {
 		HTTPClient: hc,
 	})
 	require.NoError(t, err)
+	require.NotNil(t, c)
 
-	t.Run("RegisterFailsWithInvalidInput", func(t *testing.T) {
-		out, err := c.RegisterTaskDefinition(ctx, &ecs.RegisterTaskDefinitionInput{})
-		assert.Error(t, err)
-		assert.Zero(t, out)
-	})
-	t.Run("DeregisterFailsWithInvalidInput", func(t *testing.T) {
-		out, err := c.DeregisterTaskDefinition(ctx, &ecs.DeregisterTaskDefinitionInput{})
-		assert.Error(t, err)
-		assert.Zero(t, out)
-	})
-	t.Run("RegisterAndDeregisterSucceeds", func(t *testing.T) {
-		out, err := c.RegisterTaskDefinition(ctx, &ecs.RegisterTaskDefinitionInput{
-			ContainerDefinitions: []*ecs.ContainerDefinition{
-				{
-					Command: []*string{aws.String("echo"), aws.String("hello")},
-					Image:   aws.String("ubuntu"),
-					Name:    aws.String("hello_world"),
+	for tName, tCase := range map[string]func(context.Context, *testing.T, *BasicECSClient){
+		"RegisterTaskDefinitionFailsWithInvalidInput": func(ctx context.Context, t *testing.T, c *BasicECSClient) {
+			out, err := c.RegisterTaskDefinition(ctx, &ecs.RegisterTaskDefinitionInput{})
+			assert.Error(t, err)
+			assert.Zero(t, out)
+		},
+		"DeregisterTaskDefinitionFailsWithInvalidInput": func(ctx context.Context, t *testing.T, c *BasicECSClient) {
+			out, err := c.DeregisterTaskDefinition(ctx, &ecs.DeregisterTaskDefinitionInput{})
+			assert.Error(t, err)
+			assert.Zero(t, out)
+		},
+		"RegisterAndDeregisterTaskDefinitionSucceeds": func(ctx context.Context, t *testing.T, c *BasicECSClient) {
+			registerOut, err := c.RegisterTaskDefinition(ctx, &ecs.RegisterTaskDefinitionInput{
+				ContainerDefinitions: []*ecs.ContainerDefinition{
+					{
+						Command: []*string{aws.String("echo"), aws.String("hello")},
+						Image:   aws.String("busybox"),
+						Name:    aws.String("hello_world"),
+					},
 				},
-			},
-			Cpu:    aws.String("128"),
-			Memory: aws.String("4"),
-			Family: aws.String("foo"),
+				Cpu:    aws.String("128"),
+				Memory: aws.String("4"),
+				Family: aws.String(makeFamily(t.Name())),
+			})
+			require.NoError(t, err)
+			require.NotNil(t, registerOut)
+			require.NotNil(t, registerOut.TaskDefinition)
+			require.NotNil(t, registerOut.TaskDefinition.TaskDefinitionArn)
+			require.NotZero(t, registerOut.TaskDefinition.Status)
+			assert.Equal(t, "ACTIVE", *registerOut.TaskDefinition.Status)
+			require.NotZero(t, registerOut.TaskDefinition.RegisteredAt)
+			assert.NotZero(t, *registerOut.TaskDefinition.RegisteredAt)
+
+			out, err := c.DeregisterTaskDefinition(ctx, &ecs.DeregisterTaskDefinitionInput{
+				TaskDefinition: registerOut.TaskDefinition.TaskDefinitionArn,
+			})
+			require.NoError(t, err)
+			require.NotZero(t, out)
+		},
+	} {
+		t.Run(tName, func(t *testing.T) {
+			tctx, tcancel := context.WithTimeout(ctx, 30*time.Second)
+			defer tcancel()
+
+			defer c.Close(tctx)
+
+			tCase(tctx, t, c)
 		})
-		require.NoError(t, err)
-		require.NotZero(t, out)
+	}
 
-		defer func() {
-			if out != nil && out.TaskDefinition != nil && out.TaskDefinition.TaskDefinitionArn != nil {
-				out, err := c.DeregisterTaskDefinition(ctx, &ecs.DeregisterTaskDefinitionInput{
-					TaskDefinition: out.TaskDefinition.TaskDefinitionArn,
-				})
-				require.NoError(t, err)
-				require.NotZero(t, out)
-			}
-		}()
+	registerIn := &ecs.RegisterTaskDefinitionInput{
+		ContainerDefinitions: []*ecs.ContainerDefinition{
+			{
+				Command: []*string{aws.String("echo"), aws.String("foo")},
+				Image:   aws.String("busybox"),
+				Name:    aws.String("print_foo"),
+			},
+		},
+		Cpu:    aws.String("128"),
+		Memory: aws.String("4"),
+		Family: aws.String(makeFamily(t.Name())),
+	}
 
-		require.NotZero(t, out.TaskDefinition)
-		require.NotZero(t, out.TaskDefinition.Status)
-		assert.Equal(t, "ACTIVE", *out.TaskDefinition.Status)
-		require.NotZero(t, out.TaskDefinition.RegisteredAt)
-		assert.NotZero(t, *out.TaskDefinition.RegisteredAt)
-	})
+	registerOut, err := c.RegisterTaskDefinition(ctx, registerIn)
+	require.NoError(t, err)
+	require.NotZero(t, registerOut)
+	require.NotZero(t, registerOut.TaskDefinition)
+
+	defer func() {
+		cleanupTaskDefinition(ctx, t, c, registerOut)
+		require.NoError(t, c.Close(ctx))
+	}()
+
+	for tName, tCase := range map[string]func(context.Context, *testing.T, *BasicECSClient){
+		"RunTaskFailsWithInvalidInput": func(ctx context.Context, t *testing.T, c *BasicECSClient) {
+			out, err := c.RunTask(ctx, &ecs.RunTaskInput{})
+			assert.Error(t, err)
+			assert.Zero(t, out)
+		},
+		"DescribeTasksFailsWithInvalidInput": func(ctx context.Context, t *testing.T, c *BasicECSClient) {
+			out, err := c.DescribeTasks(ctx, &ecs.DescribeTasksInput{})
+			assert.Error(t, err)
+			assert.Zero(t, out)
+		},
+		"StopTaskFailsWithInvalidInput": func(ctx context.Context, t *testing.T, c *BasicECSClient) {
+			out, err := c.StopTask(ctx, &ecs.StopTaskInput{})
+			assert.Error(t, err)
+			assert.Zero(t, out)
+		},
+		"RunTaskFailsWithValidButNonexistentInput": func(ctx context.Context, t *testing.T, c *BasicECSClient) {
+			out, err := c.RunTask(ctx, &ecs.RunTaskInput{
+				Cluster:        aws.String(os.Getenv("AWS_ECS_CLUSTER")),
+				TaskDefinition: aws.String(utility.RandomString()),
+			})
+			assert.Error(t, err)
+			assert.Zero(t, out)
+		},
+		"DescribeTasksReturnsFailureWithValidButNonexistentInput": func(ctx context.Context, t *testing.T, c *BasicECSClient) {
+			out, err := c.DescribeTasks(ctx, &ecs.DescribeTasksInput{
+				Cluster: aws.String(os.Getenv("AWS_ECS_CLUSTER")),
+				Tasks:   []*string{aws.String(utility.RandomString())},
+			})
+			assert.NoError(t, err)
+			assert.NotZero(t, out)
+			assert.NotZero(t, out.Failures)
+			assert.Empty(t, out.Tasks)
+		},
+		"StopTaskFailsWithValidButNonexistentInput": func(ctx context.Context, t *testing.T, c *BasicECSClient) {
+			out, err := c.StopTask(ctx, &ecs.StopTaskInput{
+				Cluster: aws.String(os.Getenv("AWS_ECS_CLUSTER")),
+				Task:    aws.String(utility.RandomString()),
+			})
+			assert.Error(t, err)
+			assert.Zero(t, out)
+		},
+		"RegisterSucceedsWithDuplicateTaskDefinition": func(ctx context.Context, t *testing.T, c *BasicECSClient) {
+			outDuplicate, err := c.RegisterTaskDefinition(ctx, registerIn)
+			require.NoError(t, err)
+			require.NotZero(t, outDuplicate)
+			require.NotZero(t, outDuplicate.TaskDefinition)
+
+			defer cleanupTaskDefinition(ctx, t, c, outDuplicate)
+
+			assert.True(t, *outDuplicate.TaskDefinition.Revision > *registerOut.TaskDefinition.Revision)
+		},
+		"RunAndStopTaskSucceedsWithRegisteredTaskDefinition": func(ctx context.Context, t *testing.T, c *BasicECSClient) {
+			require.NotZero(t, registerOut.TaskDefinition.Status)
+			assert.Equal(t, "ACTIVE", *registerOut.TaskDefinition.Status)
+
+			runOut, err := c.RunTask(ctx, &ecs.RunTaskInput{
+				Cluster:        aws.String(os.Getenv("AWS_ECS_CLUSTER")),
+				TaskDefinition: registerOut.TaskDefinition.TaskDefinitionArn,
+			})
+			require.NoError(t, err)
+			require.NotZero(t, runOut)
+			require.Empty(t, runOut.Failures)
+			require.NotEmpty(t, runOut.Tasks)
+			assert.Equal(t, runOut.Tasks[0].TaskDefinitionArn, registerOut.TaskDefinition.TaskDefinitionArn)
+
+			out, err := c.StopTask(ctx, &ecs.StopTaskInput{
+				Cluster: aws.String(os.Getenv("AWS_ECS_CLUSTER")),
+				Task:    aws.String(*runOut.Tasks[0].TaskArn),
+			})
+			require.NoError(t, err)
+			require.NotZero(t, out)
+			require.NotZero(t, out.Task)
+			assert.Equal(t, runOut.Tasks[0].TaskArn, out.Task.TaskArn)
+		},
+		"DescribeTaskSucceedsWithRunningTask": func(ctx context.Context, t *testing.T, c *BasicECSClient) {
+			require.NotZero(t, registerOut.TaskDefinition.Status)
+			assert.Equal(t, "ACTIVE", *registerOut.TaskDefinition.Status)
+
+			runOut, err := c.RunTask(ctx, &ecs.RunTaskInput{
+				Cluster:        aws.String(os.Getenv("AWS_ECS_CLUSTER")),
+				TaskDefinition: registerOut.TaskDefinition.TaskDefinitionArn,
+			})
+			require.NoError(t, err)
+			require.NotZero(t, runOut)
+			require.NotEmpty(t, runOut.Tasks)
+
+			defer cleanupTask(ctx, t, c, runOut)
+
+			out, err := c.DescribeTasks(ctx, &ecs.DescribeTasksInput{
+				Cluster: aws.String(os.Getenv("AWS_ECS_CLUSTER")),
+				Tasks:   []*string{aws.String(*runOut.Tasks[0].TaskArn)},
+			})
+			require.NoError(t, err)
+			require.NotZero(t, out)
+			require.NotEmpty(t, out.Tasks)
+			assert.Equal(t, *out.Tasks[0].TaskDefinitionArn, *registerOut.TaskDefinition.TaskDefinitionArn)
+			assert.Equal(t, out.Tasks[0].TaskArn, runOut.Tasks[0].TaskArn)
+		},
+	} {
+		t.Run(tName, func(t *testing.T) {
+			tctx, tcancel := context.WithTimeout(ctx, 30*time.Second)
+			defer tcancel()
+
+			tCase(tctx, t, c)
+		})
+	}
 }
 
 func checkAWSEnvVarsForECS(t *testing.T) {
@@ -102,6 +263,7 @@ func checkAWSEnvVarsForECSAndSecretsManager(t *testing.T) {
 		"AWS_REGION",
 		"AWS_ECS_CLUSTER",
 		"AWS_SECRET_PREFIX",
+		"AWS_ECS_TASK_DEFINITION_PREFIX",
 	)
 }
 
@@ -117,4 +279,8 @@ func checkEnvVars(t *testing.T, envVars ...string) {
 	if len(missing) > 0 {
 		assert.FailNow(t, fmt.Sprintf("missing required AWS environment variables: %s", missing))
 	}
+}
+
+func makeFamily(name string) string {
+	return strings.Join([]string{os.Getenv("AWS_ECS_TASK_DEFINITION_PREFIX"), "cocoa", strings.Replace(name, "/", "-", 1), utility.RandomString()}, "-")
 }
