2ac48c346bb0b5860252829b4c1a30815ae1dd9e
Kim Tao <kim.tao@mongodb.com>
2021-06-29 15:14:49 -0400

    Implement mock cluster/task and run against tests

--- mock/ecs_client.go
+++ mock/ecs_client.go
@@ -14,18 +14,6 @@ import (
 	"github.com/evergreen-ci/utility"
 )
 
-// ECSCluster represents a mock ECS cluster running tasks in the global ECS
-// service.
-type ECSCluster map[string]ECSTask
-
-// ECSTask represents a mock ECS task.
-type ECSTask struct {
-	ARN         *string
-	Cluster     *string
-	ExecEnabled *bool
-	Tags        []string
-}
-
 // ECSTaskDefinition represents a mock ECS task definition in the global ECS service.
 type ECSTaskDefinition struct {
 	ARN           *string
@@ -40,11 +28,42 @@ type ECSTaskDefinition struct {
 	Registered    *time.Time
 }
 
+func newECSTaskDefinition(def *ecs.RegisterTaskDefinitionInput, rev int) ECSTaskDefinition {
+	id := arn.ARN{
+		Partition: "aws",
+		Service:   "ecs",
+		Resource:  fmt.Sprintf("task-definition:%s/%s", utility.FromStringPtr(def.Family), strconv.Itoa(rev)),
+	}
+
+	taskDef := ECSTaskDefinition{
+		ARN:        utility.ToStringPtr(id.String()),
+		Family:     def.Family,
+		Revision:   utility.ToInt64Ptr(int64(rev)),
+		CPU:        def.Cpu,
+		MemoryMB:   def.Memory,
+		TaskRole:   def.TaskRoleArn,
+		Status:     utility.ToStringPtr(ecs.TaskDefinitionStatusActive),
+		Registered: utility.ToTimePtr(time.Now()),
+	}
+
+	taskDef.Tags = newTags(def.Tags)
+
+	for _, containerDef := range def.ContainerDefinitions {
+		if containerDef == nil {
+			continue
+		}
+		taskDef.ContainerDefs = append(taskDef.ContainerDefs, newECSContainerDefinition(containerDef))
+	}
+
+	return taskDef
+}
+
 func (d *ECSTaskDefinition) export() *ecs.TaskDefinition {
 	var containerDefs []*ecs.ContainerDefinition
 	for _, def := range d.ContainerDefs {
 		containerDefs = append(containerDefs, def.export())
 	}
+
 	return &ecs.TaskDefinition{
 		TaskDefinitionArn:    d.ARN,
 		Family:               d.Family,
@@ -54,6 +73,7 @@ func (d *ECSTaskDefinition) export() *ecs.TaskDefinition {
 		TaskRoleArn:          d.TaskRole,
 		Status:               d.Status,
 		ContainerDefinitions: containerDefs,
+		RegisteredAt:         d.Registered,
 	}
 }
 
@@ -70,29 +90,201 @@ type ECSContainerDefinition struct {
 	Tags     map[string]string
 }
 
+func newECSContainerDefinition(def *ecs.ContainerDefinition) ECSContainerDefinition {
+	return ECSContainerDefinition{
+		Name:     def.Name,
+		Image:    def.Image,
+		Command:  utility.FromStringPtrSlice(def.Command),
+		MemoryMB: def.Memory,
+		CPU:      def.Cpu,
+		EnvVars:  newEnvVars(def.Environment),
+		Secrets:  newSecrets(def.Secrets),
+	}
+}
+
 func (d *ECSContainerDefinition) export() *ecs.ContainerDefinition {
-	var env []*ecs.KeyValuePair
-	for k, v := range d.EnvVars {
-		env = append(env, &ecs.KeyValuePair{
-			Name:  utility.ToStringPtr(k),
-			Value: utility.ToStringPtr(v),
-		})
-	}
-	var tags []*ecs.Tag
-	for k, v := range d.Tags {
-		tags = append(tags, &ecs.Tag{
-			Key:   utility.ToStringPtr(k),
-			Value: utility.ToStringPtr(v),
-		})
-	}
 	return &ecs.ContainerDefinition{
 		Name:        d.Name,
 		Image:       d.Image,
 		Command:     utility.ToStringPtrSlice(d.Command),
 		Memory:      d.MemoryMB,
 		Cpu:         d.CPU,
-		Environment: env,
+		Environment: exportEnvVars(d.EnvVars),
 	}
+}
+
+// ECSCluster represents a mock ECS cluster running tasks in the global ECS
+// service.
+type ECSCluster map[string]ECSTask
+
+// ECSTask represents a mock running ECS task within a cluster.
+type ECSTask struct {
+	ARN         *string
+	TaskDef     ECSTaskDefinition
+	Cluster     *string
+	Containers  []ECSContainer
+	ExecEnabled *bool
+	Status      *string
+	GoalStatus  *string
+	Tags        map[string]string
+}
+
+func newECSTask(in *ecs.RunTaskInput, taskDef ECSTaskDefinition) ECSTask {
+	id := arn.ARN{
+		Partition: "aws",
+		Service:   "ecs",
+		Resource:  fmt.Sprintf("task:%s/%s", utility.FromStringPtr(taskDef.Family), strconv.Itoa(int(utility.FromInt64Ptr(taskDef.Revision)))),
+	}
+
+	t := ECSTask{
+		ARN:         utility.ToStringPtr(id.String()),
+		Cluster:     in.Cluster,
+		ExecEnabled: in.EnableExecuteCommand,
+		Status:      utility.ToStringPtr(ecs.DesiredStatusRunning),
+		GoalStatus:  utility.ToStringPtr(ecs.DesiredStatusRunning),
+		TaskDef:     taskDef,
+		Tags:        newTags(in.Tags),
+	}
+
+	for _, containerDef := range taskDef.ContainerDefs {
+		t.Containers = append(t.Containers, newECSContainer(containerDef, t))
+	}
+
+	return t
+}
+
+func (t *ECSTask) export() *ecs.Task {
+	exported := ecs.Task{
+		TaskArn:           t.ARN,
+		ClusterArn:        t.Cluster,
+		Tags:              exportTags(t.Tags),
+		TaskDefinitionArn: t.TaskDef.ARN,
+		Cpu:               t.TaskDef.CPU,
+		Memory:            t.TaskDef.MemoryMB,
+		LastStatus:        t.Status,
+		DesiredStatus:     t.GoalStatus,
+	}
+
+	for _, container := range t.Containers {
+		exported.Containers = append(exported.Containers, container.export())
+	}
+
+	return &exported
+}
+
+// ECSContainer represents a mock running ECS container within a task.
+type ECSContainer struct {
+	ARN      *string
+	TaskARN  *string
+	Name     *string
+	Image    *string
+	CPU      *int64
+	MemoryMB *int64
+}
+
+func newECSContainer(def ECSContainerDefinition, task ECSTask) ECSContainer {
+	name := utility.FromStringPtr(def.Name)
+	if name == "" {
+		name = utility.RandomString()
+	}
+	id := arn.ARN{
+		Partition: "aws",
+		Service:   "ecs",
+		Resource:  fmt.Sprintf("container-definition:%s-%s/%s", utility.FromStringPtr(task.TaskDef.Family), name, strconv.Itoa(int(utility.FromInt64Ptr(task.TaskDef.Revision)))),
+	}
+
+	return ECSContainer{
+		ARN:      utility.ToStringPtr(id.String()),
+		TaskARN:  task.ARN,
+		Name:     def.Name,
+		Image:    def.Image,
+		CPU:      def.CPU,
+		MemoryMB: def.MemoryMB,
+	}
+}
+
+func (c *ECSContainer) export() *ecs.Container {
+	exported := &ecs.Container{
+		ContainerArn: c.ARN,
+		TaskArn:      c.TaskARN,
+		Name:         c.Name,
+		Image:        c.Image,
+	}
+
+	if c.CPU != nil {
+		exported.Cpu = utility.ToStringPtr(strconv.Itoa(int(utility.FromInt64Ptr(c.CPU))))
+	}
+	if c.MemoryMB != nil {
+		exported.Memory = utility.ToStringPtr(strconv.Itoa(int(utility.FromInt64Ptr(c.MemoryMB))))
+	}
+
+	return exported
+}
+
+func newTags(tags []*ecs.Tag) map[string]string {
+	converted := map[string]string{}
+	for _, t := range tags {
+		if t == nil {
+			continue
+		}
+		converted[utility.FromStringPtr(t.Key)] = utility.FromStringPtr(t.Value)
+	}
+	return converted
+}
+
+func exportTags(tags map[string]string) []*ecs.Tag {
+	var exported []*ecs.Tag
+	for k, v := range tags {
+		exported = append(exported, &ecs.Tag{
+			Key:   utility.ToStringPtr(k),
+			Value: utility.ToStringPtr(v),
+		})
+	}
+	return exported
+}
+
+func newEnvVars(envVars []*ecs.KeyValuePair) map[string]string {
+	converted := map[string]string{}
+	for _, envVar := range envVars {
+		if envVar == nil {
+			continue
+		}
+		converted[utility.FromStringPtr(envVar.Name)] = utility.FromStringPtr(envVar.Value)
+	}
+	return converted
+}
+
+func exportEnvVars(envVars map[string]string) []*ecs.KeyValuePair {
+	var exported []*ecs.KeyValuePair
+	for k, v := range envVars {
+		exported = append(exported, &ecs.KeyValuePair{
+			Name:  utility.ToStringPtr(k),
+			Value: utility.ToStringPtr(v),
+		})
+	}
+	return exported
+}
+
+func newSecrets(secrets []*ecs.Secret) map[string]string {
+	converted := map[string]string{}
+	for _, secret := range secrets {
+		if secret == nil {
+			continue
+		}
+		converted[utility.FromStringPtr(secret.Name)] = utility.FromStringPtr(secret.ValueFrom)
+	}
+	return converted
+}
+
+func exportSecrets(secrets map[string]string) []*ecs.Secret {
+	var exported []*ecs.Secret
+	for k, v := range secrets {
+		exported = append(exported, &ecs.Secret{
+			Name:      utility.ToStringPtr(k),
+			ValueFrom: utility.ToStringPtr(v),
+		})
+	}
+	return exported
 }
 
 // ECSService is a global implementation of ECS that provides a simplified
@@ -101,7 +293,6 @@ func (d *ECSContainerDefinition) export() *ecs.ContainerDefinition {
 // indirectly with the ECSClient to access or modify ECS resources, or used
 // directly.
 type ECSService struct {
-	// kim: TODO: need mutexes for multi-threaded?
 	Clusters map[string]ECSCluster
 	TaskDefs map[string][]ECSTaskDefinition
 }
@@ -125,6 +316,23 @@ type ECSClient struct {
 
 	DeregisterTaskDefinitionInput  *ecs.DeregisterTaskDefinitionInput
 	DeregisterTaskDefinitionOutput *ecs.DeregisterTaskDefinitionOutput
+
+	ListTaskDefinitionsInput  *ecs.ListTaskDefinitionsInput
+	ListTaskDefinitionsOutput *ecs.ListTaskDefinitionsOutput
+
+	RunTaskInput  *ecs.RunTaskInput
+	RunTaskOutput *ecs.RunTaskOutput
+
+	DescribeTasksInput  *ecs.DescribeTasksInput
+	DescribeTasksOutput *ecs.DescribeTasksOutput
+
+	ListTasksInput  *ecs.ListTasksInput
+	ListTasksOutput *ecs.ListTasksOutput
+
+	StopTaskInput  *ecs.StopTaskInput
+	StopTaskOutput *ecs.StopTaskOutput
+
+	CloseError error
 }
 
 // RegisterTaskDefinition saves the input and returns a new mock task
@@ -137,48 +345,14 @@ func (c *ECSClient) RegisterTaskDefinition(ctx context.Context, in *ecs.Register
 		return c.RegisterTaskDefinitionOutput, nil
 	}
 
+	if in.Family == nil {
+		return nil, errors.New("missing family")
+	}
+
 	revisions := GlobalECSService.TaskDefs[utility.FromStringPtr(in.Family)]
 	rev := len(revisions) + 1
 
-	id := arn.ARN{
-		Partition: "aws",
-		Service:   "ecs",
-		Resource:  fmt.Sprintf("%s:%s", utility.FromStringPtr(in.Family), strconv.Itoa(rev)),
-	}
-
-	taskDef := ECSTaskDefinition{
-		ARN:        utility.ToStringPtr(id.String()),
-		Family:     in.Family,
-		Revision:   utility.ToInt64Ptr(int64(rev)),
-		CPU:        in.Cpu,
-		MemoryMB:   in.Memory,
-		TaskRole:   in.TaskRoleArn,
-		Tags:       map[string]string{},
-		Status:     utility.ToStringPtr(ecs.TaskDefinitionStatusActive),
-		Registered: utility.ToTimePtr(time.Now()),
-	}
-
-	for _, t := range in.Tags {
-		if t == nil {
-			continue
-		}
-		taskDef.Tags[utility.FromStringPtr(t.Key)] = utility.FromStringPtr(t.Value)
-	}
-	for _, def := range in.ContainerDefinitions {
-		containerDef := ECSContainerDefinition{
-			Name:     def.Name,
-			Image:    def.Image,
-			Command:  utility.FromStringPtrSlice(def.Command),
-			MemoryMB: def.Memory,
-			CPU:      def.Cpu,
-			EnvVars:  map[string]string{},
-			Secrets:  map[string]string{},
-		}
-		for _, s := range def.Secrets {
-			containerDef.EnvVars[utility.FromStringPtr(s.Name)] = utility.FromStringPtr(s.ValueFrom)
-		}
-		taskDef.ContainerDefs = append(taskDef.ContainerDefs, containerDef)
-	}
+	taskDef := newECSTaskDefinition(in, rev)
 
 	GlobalECSService.TaskDefs[utility.FromStringPtr(in.Family)] = append(revisions, taskDef)
 
@@ -198,59 +372,85 @@ func (c *ECSClient) DeregisterTaskDefinition(ctx context.Context, in *ecs.Deregi
 		return c.DeregisterTaskDefinitionOutput, nil
 	}
 
+	if in.TaskDefinition == nil {
+		return nil, errors.New("missing task definition")
+	}
+
 	id := utility.FromStringPtr(in.TaskDefinition)
 
-	var taskDef ECSTaskDefinition
 	if arn.IsARN(id) {
-		var found bool
-		for family, revisions := range GlobalECSService.TaskDefs {
-			for revNum, def := range revisions {
-				if utility.FromStringPtr(def.ARN) == id {
-					taskDef = def
-					taskDef.Status = utility.ToStringPtr(ecs.TaskDefinitionStatusInactive)
-					GlobalECSService.TaskDefs[family][revNum] = taskDef
-					break
-				}
-			}
-		}
+		family, revNum, found := taskDefIndexFromARN(id)
 		if !found {
-			return nil, errors.New("resource not found")
+			return nil, errors.New("task definition not found")
 		}
-	} else {
-		splitIdx := strings.LastIndex(id, ":")
-		if splitIdx == -1 {
-			return nil, errors.New("malformed task definition input")
-		}
-
-		family := id[:splitIdx]
-
-		revNum, err := strconv.Atoi(id[splitIdx+1:])
-		if err != nil {
-			return nil, errors.Wrap(err, "parsing revision")
-		}
-
-		revisions, ok := GlobalECSService.TaskDefs[family]
-		if !ok {
-			return nil, errors.New("family not found")
-		}
-		if len(revisions) < revNum {
-			return nil, errors.New("revision not found")
-		}
-
-		taskDef = revisions[revNum]
+		taskDef := GlobalECSService.TaskDefs[family][revNum-1]
 		taskDef.Status = utility.ToStringPtr(ecs.TaskDefinitionStatusInactive)
-		GlobalECSService.TaskDefs[family][revNum] = taskDef
+		GlobalECSService.TaskDefs[family][revNum-1] = taskDef
+
+		return &ecs.DeregisterTaskDefinitionOutput{
+			TaskDefinition: taskDef.export(),
+		}, nil
 	}
+
+	family, revNum, err := parseFamilyAndRevision(id)
+	if err != nil {
+		return nil, errors.Wrap(err, "invalid task definition")
+	}
+
+	revisions, ok := GlobalECSService.TaskDefs[family]
+	if !ok {
+		return nil, errors.New("family not found")
+	}
+	if len(revisions) < revNum {
+		return nil, errors.New("revision not found")
+	}
+
+	taskDef := revisions[revNum-1]
+	taskDef.Status = utility.ToStringPtr(ecs.TaskDefinitionStatusInactive)
+	GlobalECSService.TaskDefs[family][revNum-1] = taskDef
 
 	return &ecs.DeregisterTaskDefinitionOutput{
 		TaskDefinition: taskDef.export(),
 	}, nil
 }
 
+func parseFamilyAndRevision(taskDef string) (family string, revNum int, err error) {
+	partition := strings.LastIndex(taskDef, ":")
+	if partition == -1 {
+		return "", -1, errors.New("task definition is not in family:revision format")
+	}
+
+	family = taskDef[:partition]
+
+	revNum, err = strconv.Atoi(taskDef[partition+1:])
+	if err != nil {
+		return "", -1, errors.Wrap(err, "parsing revision")
+	}
+
+	return family, revNum, nil
+}
+
+func taskDefIndexFromARN(arn string) (family string, revNum int, found bool) {
+	for family, revisions := range GlobalECSService.TaskDefs {
+		for revIdx, def := range revisions {
+			if utility.FromStringPtr(def.ARN) == arn {
+				return family, revIdx + 1, true
+			}
+		}
+	}
+	return "", -1, false
+}
+
 // ListTaskDefinitions saves the input and lists all matching task definitions.
 // The mock output can be customized. By default, it will list all cached task
 // definitions that match the input filters.
 func (c *ECSClient) ListTaskDefinitions(ctx context.Context, in *ecs.ListTaskDefinitionsInput) (*ecs.ListTaskDefinitionsOutput, error) {
+	c.ListTaskDefinitionsInput = in
+
+	if c.ListTaskDefinitionsOutput != nil {
+		return c.ListTaskDefinitionsOutput, nil
+	}
+
 	var arns []*string
 	for _, revisions := range GlobalECSService.TaskDefs {
 		for _, def := range revisions {
@@ -264,6 +464,7 @@ func (c *ECSClient) ListTaskDefinitions(ctx context.Context, in *ecs.ListTaskDef
 			arns = append(arns, def.ARN)
 		}
 	}
+
 	return &ecs.ListTaskDefinitionsOutput{
 		TaskDefinitionArns: arns,
 	}, nil
@@ -273,32 +474,187 @@ func (c *ECSClient) ListTaskDefinitions(ctx context.Context, in *ecs.ListTaskDef
 // definition. The mock output can be customized. By default, it will create
 // mock output based on the input.
 func (c *ECSClient) RunTask(ctx context.Context, in *ecs.RunTaskInput) (*ecs.RunTaskOutput, error) {
-	return nil, errors.New("TODO: implement")
+	c.RunTaskInput = in
+
+	if c.RunTaskOutput != nil {
+		return c.RunTaskOutput, nil
+	}
+
+	if in.TaskDefinition == nil {
+		return nil, errors.New("missing task definition")
+	}
+
+	clusterName := c.getOrDefaultCluster(in.Cluster)
+	cluster, ok := GlobalECSService.Clusters[clusterName]
+	if !ok {
+		return nil, errors.New("cluster not found")
+	}
+
+	taskDefID := utility.FromStringPtr(in.TaskDefinition)
+
+	if arn.IsARN(taskDefID) {
+		family, revNum, found := taskDefIndexFromARN(taskDefID)
+		if !found {
+			return nil, errors.New("task definition not found")
+		}
+		taskDef := GlobalECSService.TaskDefs[family][revNum-1]
+		task := newECSTask(in, taskDef)
+
+		cluster[utility.FromStringPtr(task.ARN)] = task
+
+		return &ecs.RunTaskOutput{
+			Tasks: []*ecs.Task{task.export()},
+		}, nil
+	}
+
+	family, revNum, err := parseFamilyAndRevision(taskDefID)
+
+	var taskDef ECSTaskDefinition
+	if err == nil {
+		revisions, ok := GlobalECSService.TaskDefs[family]
+		if !ok {
+			return nil, errors.New("task definition family not found")
+		}
+		if len(revisions) < revNum {
+			return nil, errors.New("task definition revision not found")
+		}
+
+		taskDef = revisions[revNum-1]
+	} else {
+		// Use the latest revision if none is specified.
+		family = taskDefID
+		revisions, ok := GlobalECSService.TaskDefs[family]
+		if !ok {
+			return nil, errors.New("task definition family not found")
+		}
+
+		revNum = len(revisions)
+		taskDef = revisions[revNum-1]
+	}
+
+	task := newECSTask(in, taskDef)
+
+	cluster[utility.FromStringPtr(task.ARN)] = task
+
+	return &ecs.RunTaskOutput{
+		Tasks: []*ecs.Task{task.export()},
+	}, nil
+}
+
+func (c *ECSClient) getOrDefaultCluster(name *string) string {
+	if name == nil {
+		return "default"
+	}
+	return *name
 }
 
 // DescribeTasks saves the input and returns information about the existing
 // tasks. The mock output can be customized. By default, it will describe all
 // cached tasks that match.
 func (c *ECSClient) DescribeTasks(ctx context.Context, in *ecs.DescribeTasksInput) (*ecs.DescribeTasksOutput, error) {
-	return nil, errors.New("TODO: implement")
+	c.DescribeTasksInput = in
+
+	if c.DescribeTasksOutput != nil {
+		return c.DescribeTasksOutput, nil
+	}
+
+	cluster, ok := GlobalECSService.Clusters[c.getOrDefaultCluster(in.Cluster)]
+	if !ok {
+		return nil, errors.New("cluster not found")
+	}
+
+	ids := utility.FromStringPtrSlice(in.Tasks)
+
+	var tasks []*ecs.Task
+	var failures []*ecs.Failure
+	for _, id := range ids {
+		task, ok := cluster[id]
+		if !ok {
+			failures = append(failures, &ecs.Failure{
+				Arn:    utility.ToStringPtr(id),
+				Reason: utility.ToStringPtr("task not found"),
+			})
+			continue
+		}
+
+		tasks = append(tasks, task.export())
+	}
+
+	return &ecs.DescribeTasksOutput{
+		Tasks:    tasks,
+		Failures: failures,
+	}, nil
 }
 
 // ListTasks saves the input and lists all matching tasks. The mock output can
 // be customized. By default, it will list all cached task definitions that
 // match the input filters.
 func (c *ECSClient) ListTasks(ctx context.Context, in *ecs.ListTasksInput) (*ecs.ListTasksOutput, error) {
-	return nil, errors.New("TODO: implement")
+	c.ListTasksInput = in
+
+	if c.ListTasksOutput != nil {
+		return c.ListTasksOutput, nil
+	}
+
+	cluster, ok := GlobalECSService.Clusters[c.getOrDefaultCluster(in.Cluster)]
+	if !ok {
+		return nil, errors.New("cluster not found")
+	}
+
+	var arns []string
+	for arn, task := range cluster {
+		if in.DesiredStatus != nil && utility.FromStringPtr(task.GoalStatus) != *in.DesiredStatus {
+			continue
+		}
+
+		if in.Family != nil && utility.FromStringPtr(task.TaskDef.Family) != *in.Family {
+			continue
+		}
+
+		arns = append(arns, arn)
+	}
+
+	return &ecs.ListTasksOutput{
+		TaskArns: utility.ToStringPtrSlice(arns),
+	}, nil
 }
 
 // StopTask saves the input and stops a mock task. The mock output can be
 // customized. By default, it will mark a cached task as stopped if it exists
 // and is running.
 func (c *ECSClient) StopTask(ctx context.Context, in *ecs.StopTaskInput) (*ecs.StopTaskOutput, error) {
-	return nil, errors.New("TODO: implement")
+	c.StopTaskInput = in
+
+	if c.StopTaskOutput != nil {
+		return c.StopTaskOutput, nil
+	}
+
+	cluster, ok := GlobalECSService.Clusters[c.getOrDefaultCluster(in.Cluster)]
+	if !ok {
+		return nil, errors.New("cluster not found")
+	}
+
+	task, ok := cluster[utility.FromStringPtr(in.Task)]
+	if !ok {
+		return nil, errors.New("task not found")
+	}
+
+	task.Status = utility.ToStringPtr(ecs.DesiredStatusStopped)
+	task.GoalStatus = utility.ToStringPtr(ecs.DesiredStatusStopped)
+
+	cluster[utility.FromStringPtr(in.Task)] = task
+
+	return &ecs.StopTaskOutput{
+		Task: task.export(),
+	}, nil
 }
 
 // Close closes the mock client. The mock output can be customized. By default,
 // it is a no-op that returns no error.
 func (c *ECSClient) Close(ctx context.Context) error {
+	if c.CloseError != nil {
+		return c.CloseError
+	}
+
 	return nil
 }
--- mock/secrets_manager_client_test.go
+++ mock/secrets_manager_client_test.go
@@ -7,7 +7,6 @@ import (
 
 	"github.com/evergreen-ci/cocoa"
 	"github.com/evergreen-ci/cocoa/internal/testcase"
-	"github.com/evergreen-ci/utility"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -19,14 +18,13 @@ func TestSecretsManagerClient(t *testing.T) {
 
 	for tName, tCase := range testcase.SecretsManagerClientTests() {
 		t.Run(tName, func(t *testing.T) {
-			tctx, tcancel := context.WithTimeout(ctx, 30*time.Second)
+			tctx, tcancel := context.WithTimeout(ctx, time.Second)
 			defer tcancel()
 
-			hc := utility.GetHTTPClient()
-			defer utility.PutHTTPClient(hc)
-
 			c := &SecretsManagerClient{}
-			defer c.Close(tctx)
+			defer func() {
+				assert.NoError(t, c.Close(tctx))
+			}()
 
 			tCase(tctx, t, c)
 		})
