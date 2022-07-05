5eae263a1a94be28c117bc303e8f1b1da19d203b
Kim Tao <kim.tao@mongodb.com>
2022-07-05 14:13:00 -0400

    Add doc comments for new pod definition manager and cache

--- ecs/pod_definition_cache.go
+++ ecs/pod_definition_cache.go
@@ -8,21 +8,20 @@ import (
 	"github.com/pkg/errors"
 )
 
+// PodDefinitionCache represents an external cache that tracks pod definitions.
 type PodDefinitionCache interface {
 	Put(ctx context.Context, res cocoa.ECSPodDefinitionOptions) error
 }
 
+// PodDefinitionManager manages pod definitions, which are configuration
+// templates used to run pods. It can be optionally backed by an external
+// PodDefinitionCache to keep track of the pod definitions.
 type PodDefinitionManager struct {
-	cache PodDefinitionCache
+	client cocoa.ECSClient    //nolint
+	cache  PodDefinitionCache //nolint
 }
 
 // CreatePodDefinition creates a pod definition and caches it.
-// kim: TODO: pass PodDefinitionOptions to CreatePodDefinition
 func (m *PodDefinitionManager) CreatePodDefinition(ctx context.Context, opts ...cocoa.ECSPodDefinitionOptions) (*ecs.TaskDefinition, error) {
-	// kim: TODO: register task definition with "untracked" tag.
-
-	// kim: TODO: cache task definition.
-
-	// kim: TODO: re-tag task definition with "tracked" tag.
-	return nil, errors.New("kim: TODO: implement")
+	return nil, errors.New("TODO: implement")
 }
